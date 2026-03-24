c74929709f763cfa4334ce709a2db6a536c05875
champagnebrg <lucaborgonovi1@gmail.com>
2026-03-24 12:55:43 +0100

    Fix DEV_HOSTS syntax in firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,7 +6,7 @@ const DEV_HOSTS = new Set([
   "127.0.0.1",
   "climby-dev-1.web.app",
   "climby-dev-1.firebaseapp.com",
-  "champagnebrg.github.io",
+  "champagnebrg.github.io"
 ]);
 
 const PROD_PLACEHOLDER_PREFIX = "__SET_PROD_";
